908ae9828d446bb303cb3c46e77284b12f7721b9
Dinesh Thakur <66509841+Dinex-dev@users.noreply.github.com>
2022-02-22 18:43:06 +0530

    A basic quiz using javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+var qno=0
+var x;
+const url = "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
+fetch(url).then(function (response){
+    return response.json();
+}).then(function (data){
+    document.getElementById("question").innerHTML='Q.'+ (qno+1)+' ' +data.results[qno].question;
+    document.getElementById("option1").innerHTML=data.results[qno].correct_answer;
+    document.getElementById("option2").innerHTML=data.results[qno].incorrect_answers[0];
+    document.getElementById("option3").innerHTML=data.results[qno].incorrect_answers[1];
+    document.getElementById("option4").innerHTML=data.results[qno].incorrect_answers[2];
+    //document.getElementById("disp").className = 'container display';
+    //document.getElementById("loader").className = 'hidden';
+    console.log(data.results);
+}).catch(function (error){
+    console.log(error);
+}
+)
